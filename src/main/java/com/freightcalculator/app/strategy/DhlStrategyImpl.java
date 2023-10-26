5fcbc8712a1b85eb6164c901252a0b8d914075ed
Andre <parelhoneves.andre@gmail.com>
2023-10-25 22:44:19 -0300

    fix

--- src/main/java/com/freightcalculator/app/strategy/DhlStrategyImpl.java
+++ src/main/java/com/freightcalculator/app/strategy/DhlStrategyImpl.java
@@ -3,9 +3,8 @@ package com.freightcalculator.app.strategy;
 import com.freightcalculator.app.model.Package;
 import org.springframework.stereotype.Service;
 
-@Service("correios")
+@Service("dhl")
 public class DhlStrategyImpl implements ShipmentStrategy {
-
     private final double coefficient = 0.1;
     private final double valueRandom = 5;
     private final double internacionalCoefficient = 6000;
